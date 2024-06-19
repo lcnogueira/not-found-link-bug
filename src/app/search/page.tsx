8f5f8174d511eb4ffe624d42769af0c2dadf3589
lcnogueira <luizcns18@gmail.com>
2024-06-19 13:29:42 -0300

    throw error from within search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,15 @@
-export default function Search({searchParams}: {searchParams: {q: string}}){
+import { notFound } from "next/navigation";
 
+export const runtime = 'edge';
+
+export const dynamic = 'force-dynamic';
+
+export default function Search({searchParams}: {searchParams: {q: string}}){
+    const {q} = searchParams;
+    if(q === 'error'){
+        return notFound();
+    }
+    
     return (
         <div>
             <h1>Search for: {searchParams.q}</h1>
